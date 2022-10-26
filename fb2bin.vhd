8ec33848c282910847ba9a4c4b795d154f7d438b
Oryam <59542576+T3co@users.noreply.github.com>
2022-10-26 14:17:25 +0300

    Add files via upload

--- fb2bin.vhd
+++ fb2bin.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity fb2bin is
-port ( number : in integer range 0 to 4;
+port ( number : in bit_vector (7 downto 0);
 	   Ma0: buffer bit;
 		Ma1: buffer bit;
 		Mb0: buffer bit;
@@ -15,30 +15,30 @@ begin
 	begin
 	
 	------------------------------------- 0000 stop
-	if number = 0 then Ma0 <= '0'; end if;
-	if number = 0 then Ma1 <= '0'; end if;
-	if number = 0 then Mb0 <= '0'; end if;
-	if number = 0 then Mb1 <= '0'; end if;
+	if number = "00000000" then Ma0 <= '0'; end if;
+	if number = "00000000" then Ma1 <= '0'; end if;
+	if number = "00000000" then Mb0 <= '0'; end if;
+	if number = "00000000" then Mb1 <= '0'; end if;
 	------------------------------------- 0101 forwards
-	if number = 1 then Ma0 <= '0'; end if;
-	if number = 1 then Ma1 <= '1'; end if;
-	if number = 1 then Mb0 <= '0'; end if;
-	if number = 1 then Mb1 <= '1'; end if;
+	if number = "00000101" then Ma0 <= '0'; end if;
+	if number = "00000101" then Ma1 <= '1'; end if;
+	if number = "00000101" then Mb0 <= '0'; end if;
+	if number = "00000101" then Mb1 <= '1'; end if;
 	------------------------------------- 1010 backwards
-	if number = 2 then Ma0 <= '1'; end if;
-	if number = 2 then Ma1 <= '0'; end if;
-	if number = 2 then Mb0 <= '1'; end if;
-	if number = 2 then Mb1 <= '0'; end if;
+	if number = "00001010" then Ma0 <= '1'; end if;
+	if number = "00001010" then Ma1 <= '0'; end if;
+	if number = "00001010" then Mb0 <= '1'; end if;
+	if number = "00001010" then Mb1 <= '0'; end if;
 	------------------------------------- 0110 right
-	if number = 3 then Ma0 <= '0'; end if;
-	if number = 3 then Ma1 <= '1'; end if;
-	if number = 3 then Mb0 <= '1'; end if;
-	if number = 3 then Mb1 <= '0'; end if;
+	if number = "00000110" then Ma0 <= '0'; end if;
+	if number = "00000110" then Ma1 <= '1'; end if;
+	if number = "00000110" then Mb0 <= '1'; end if;
+	if number = "00000110" then Mb1 <= '0'; end if;
 	------------------------------------- 1001 left
-	if number = 4 then Ma0 <= '1'; end if;
-	if number = 4 then Ma1 <= '0'; end if;
-	if number = 4 then Mb0 <= '0'; end if;
-	if number = 4 then Mb1 <= '1'; end if;
+	if number = "00001001" then Ma0 <= '1'; end if;
+	if number = "00001001" then Ma1 <= '0'; end if;
+	if number = "00001001" then Mb0 <= '0'; end if;
+	if number = "00001001" then Mb1 <= '1'; end if;
  end process;
 
 end behave;
