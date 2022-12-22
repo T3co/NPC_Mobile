e4e72b0323aed1fae3ed001285f74315077a0eb1
Korzack <90255768+idkz712@users.noreply.github.com>
2022-12-22 17:10:16 +0200

    PWM speed update

--- Altera Tutorial/PWM_Motor.vhd
+++ Altera Tutorial/PWM_Motor.vhd
@@ -2,9 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity PWM_Motor is
 port ( clk: in bit;
-		 motorInput : in bit_vector(3 downto 0);
+		 motorInput : in bit_vector(2 downto 0);
+		 speedMotor : in bit;
 		 pOut : out bit;
-		 motorOutput : out bit_vector(3 downto 0));
+		 motorOutput : out bit_vector(2 downto 0));
 end;
 architecture RTL of PWM_Motor is
   signal pulse : bit ;
@@ -12,13 +13,12 @@ architecture RTL of PWM_Motor is
   
 begin 
 --checks if the car is going left or right to reduce the speed by half(aka making the pwm 40% and not 80%)
-cntMax<=2500 when motorInput="0011" else 
-		  2500 when motorInput="0100" else
-		  5000;
+cnt80<=1000 when motorInput="011" else
+	    1000 when motorInput="100" else
+	    2000 when speedMotor = '0' and (motorInput = "001" or motorInput = "010") else
+		 4000 when speedMotor = '1' and (motorInput = "001" or motorInput = "010");	
+		 
 
-cnt80<=2000 when motorInput="0011" else
-	    2000 when motorInput="0100" else
-	    4000;
   process (clk)
   begin 
   if clk'event and clk = '1' then --clock if
--- Altera Tutorial/data_spliter.vhd
+++ Altera Tutorial/data_spliter.vhd
@@ -2,14 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity data_spliter is
 port (data : in bit_vector(7 downto 0);
-	  motor : out bit_vector(3 downto 0);
-		servo : out bit_vector(6 downto 4);
+		motor : out bit_vector(2 downto 0);
+		servo : out bit_vector(5 downto 3);
+		speedMotor : out bit;
 		Laser : out bit);
 end;
 architecture behave of data_spliter is
 begin
-motor<=data(3 downto 0);
+motor<=data(2 downto 0);
 servo<=data(6 downto 4);
+speedMotor<=data(6);
 Laser<=data(7);
 
 end behave;
