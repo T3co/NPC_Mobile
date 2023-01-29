23aa4af3743a76302d03fe8379f8b29479dc7dbd
Korzack <90255768+idkz712@users.noreply.github.com>
2023-01-29 21:26:14 +0200

    ESP32 and webserver merger

--- Arduino/espserver/espserver.ino
+++ Arduino/espserver/espserver.ino
@@ -1,10 +1,19 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Arduino.h>
+#include <WiFiMulti.h>
 #include "website.h"
 
+#define RXD2 16
+#define TXD2 17
+
+#define ledpin 2
 
 #define LOCAL_SSID "upstairs"
 #define LOCAL_PASSWORD "10203040"
+
+WiFiMulti wifiMulti;
+
 
 WebServer server(80);
 char XML[512];
@@ -12,20 +21,63 @@ char buf[32];
 
 int temp = 20;
 int packet = 0;
+bool a = false;
+
+
 
 void setup() {
+
   Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  pinMode(ledpin, OUTPUT);
+
+  wifiMulti.addAP("Ams_2.4GHz", "0523993253A");
+  wifiMulti.addAP("ORYAM", "12345678");
+  wifiMulti.addAP("upstairs", "10203040");
+  wifiMulti.addAP("DARCA", "");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } 
+  else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+      
+  }
 
   Serial.println("Starting Server");
+if(wifiMulti.run() != WL_CONNECTED) {
+  Serial.println("WiFi not connected!");
+  delay(1000);
+}
 
-  WiFi.begin(LOCAL_SSID, LOCAL_PASSWORD);
-  Serial.println("Connecting To WiFi...");
-
-  while(WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  Serial.println("");
+  Serial.print("Connecting To: " );
+  Serial.print(WiFi.SSID());
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  String ipAddress = WiFi.localIP().toString();;
+  Serial.println(ipAddress);
+
 
   Serial.println("IP address :"); Serial.print(WiFi.localIP());
 
@@ -64,4 +116,12 @@ void upacket(){
   strcat(XML, buf);
   strcat(XML, "</Data>\n");  
   server.send(200, "text/plain", buf);
+}
+
+void blink() {
+  a = not a;
+  if (a)
+    digitalWrite(ledpin, HIGH);
+  else
+    digitalWrite(ledpin, LOW);
 }
