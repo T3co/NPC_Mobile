da5c4fad1fff6f0577104c3a889aabe69832fda6
Oryam_Dev <Oryambibi@gmail.com>
2023-04-25 14:29:23 +0300

    All working except camera and its led

--- Altera Tutorial/Motor_Ctrl.vhd
+++ Altera Tutorial/Motor_Ctrl.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity Motor_Ctrl is
 port(PWM_Motor_1KHz: in bit;
-		Motor_Bits: in bit_vector (3 downto 0);
+		Motor_Bits: in bit_vector (2 downto 0);
 		LwheelB: buffer bit;
 		LwheelF: buffer bit;
 		RwheelB: buffer bit;
@@ -14,19 +14,19 @@ begin
 process(PWM_Motor_1KHz,Motor_Bits)
 begin
 -------------------------------------------------------------------- no movement
-if Motor_Bits = "0000" then LwheelB <= '0'; LwheelF <= '0'; RwheelB <= '0'; RwheelF <= '0'; end if;
+if Motor_Bits = "000" then LwheelB <= '0'; LwheelF <= '0'; RwheelB <= '0'; RwheelF <= '0'; end if;
 
 -------------------------------------------------------------------- forward
-if Motor_Bits = "0101" then LwheelB <= '0'; LwheelF <= PWM_Motor_1KHz; RwheelB <= '0'; RwheelF <= PWM_Motor_1KHz; end if;
+if Motor_Bits = "100" then LwheelB <= '0'; LwheelF <= PWM_Motor_1KHz; RwheelF <= PWM_Motor_1KHz; RwheelB <= '0'; end if;
 
 -------------------------------------------------------------------- backward :)
-if Motor_Bits = "1010" then LwheelB <= PWM_Motor_1KHz; LwheelF <= '0'; RwheelB <= PWM_Motor_1KHz; RwheelF <= '0'; end if;
+if Motor_Bits = "011" then LwheelB <= PWM_Motor_1KHz; LwheelF <= '0'; RwheelF <= '0'; RwheelB <= PWM_Motor_1KHz; end if;
 
 -------------------------------------------------------------------- right
-if Motor_Bits = "0110" then LwheelB <= '0'; LwheelF <= PWM_Motor_1KHz; RwheelB <= PWM_Motor_1KHz; RwheelF <= '0'; end if;
+if Motor_Bits = "001" then LwheelB <= '0'; LwheelF <= PWM_Motor_1KHz; RwheelB <= '0'; RwheelF <= '0'; end if;
 
 -------------------------------------------------------------------- left
-if Motor_Bits = "1001" then LwheelB <= PWM_Motor_1KHz; LwheelF <= '0'; RwheelB <= '0'; RwheelF <= PWM_Motor_1KHz; end if;
+if Motor_Bits = "010" then LwheelB <= '0'; LwheelF <= '0'; RwheelF <= PWM_Motor_1KHz; RwheelB <= '0'; end if;
 
 
 end process;
--- Altera Tutorial/output_files/Altera_Tutorial.cdf
+++ Altera Tutorial/output_files/Altera_Tutorial.cdf
@@ -3,8 +3,8 @@ JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
 
-	P ActionCode(Ign)
-		Device PartName(10M50DAF484) MfrSpec(OpMask(0) Child_OpMask(2 1 1) FullPath("D:/GitHub/NPC_Mobile/Altera Tutorial/output_files/Altera_Tutorial.pof"));
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("D:/GitHub/NPC_Mobile/Altera Tutorial/output_files/") File("Altera_Tutorial.pof") MfrSpec(OpMask(1) Child_OpMask(2 1 1));
 
 ChainEnd;
 
